1e44ccb2004bc88fbd774f0c8dd6f26086e801d5
terrytykuo <oldiegoodie99@gmail.com>
2025-10-08 06:49:08 +0200

    offline devbot

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 // 導入所有 bots
 const vibeBot = require('./bots/vibe-bot');
 const poBot = require('./bots/po-bot');
-const devBot = require('./bots/dev-bot');
+// const devBot = require('./bots/dev-bot');
 
-console.log('🚀 Starting all bots...\n');
+console.log('🚀 Starting bots...\n');
 
-// 啟動所有 bots
+// 啟動 bots
 vibeBot.start();
 poBot.start();
-devBot.start();
+// devBot.start();
 
-console.log('\n✨ All bots are starting up...');
+console.log('\n✨ Bots are starting up...');
 console.log('📝 Check logs above for individual bot status.\n');
